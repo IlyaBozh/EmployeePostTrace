30529f79c175182e4493b4e43890f7e32ee328bb
Ilya <ilia.il.27@mail.ru>
2023-08-10 21:00:46 +0300

    Create data tables

---
+++ EmployeePostTrace.DB/dbo/Tables/Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id]               INT IDENTITY(1,1)     NOT NULL PRIMARY KEY,
+	[FirstName]        NCHAR (50)            NOT NULL,
+    [LastName]         NCHAR (50)            NOT NULL,
+    [Patronymic]       NCHAR (50)            NOT NULL,
+	[Email]            NCHAR (50)            NOT NULL,
+	[Password]         NCHAR (255)           NOT NULL,
+	[IsDeleted]        BIT DEFAULT 0         NOT NULL
+)
---
+++ EmployeePostTrace.DB/dbo/Tables/Letter.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Letter]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Header] NCHAR (50)            NOT NULL,
+	[Sender] NCHAR (50) NOT NULL,
+	[Recipient] NCHAR (50) NOT NULL,
+	[Content] NCHAR (50) NOT NULL,
+	[SendingDate] DATETIME NOT NULL
+)
