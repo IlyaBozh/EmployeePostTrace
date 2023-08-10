5cecda69d1a95eb052c49ade0af5a7ac5de5f827
Ilya <ilia.il.27@mail.ru>
2023-08-11 01:14:12 +0300

    Add repository EmployeeRepository

--- EmployeePostTrace.DataLayer/Models/EmployeeDto.cs
+++ EmployeePostTrace.DataLayer/Models/EmployeeDto.cs
@@ -5,7 +5,7 @@ public class EmployeeDto
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
-    public string lastName { get; set; }
+    public string LastName { get; set; }
     public string Patronymic { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
--- EmployeePostTrace.DataLayer/Repositories/Interfaces/IEmployeeRepository.cs
+++ EmployeePostTrace.DataLayer/Repositories/Interfaces/IEmployeeRepository.cs
@@ -8,5 +8,5 @@ public interface IEmployeeRepository
     Task<int> Add(EmployeeDto employeeDto);
     Task Delete(int id, bool isDeleted);
     Task<EmployeeDto> GetById(int id);
-    Task<EmployeeDto> Update(EmployeeDto employeeDto);
+    Task Update(EmployeeDto employeeDto);
 }
