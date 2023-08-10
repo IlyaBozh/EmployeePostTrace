eb75ba3b9bfb13f9634788355706c3dc27601b08
Ilya <ilia.il.27@mail.ru>
2023-08-11 00:44:21 +0300

    Add interface IEmployeeRepository

---
+++ EmployeePostTrace.DataLayer/Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using EmployeePostTrace.DataLayer.Models;
+
+namespace EmployeePostTrace.DataLayer.Repositories.Interfaces;
+
+public interface IEmployeeRepository
+{
+    Task<int> Add(EmployeeDto employeeDto);
+    Task Delete(int id, bool isDeleted);
+    Task<EmployeeDto> GetById(int id);
+    Task<EmployeeDto> Update(EmployeeDto employeeDto);
+}
