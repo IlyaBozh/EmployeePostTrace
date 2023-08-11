01d3cdff1af6f08f0bbd8891928f395f246c2066
Ilya <ilia.il.27@mail.ru>
2023-08-12 01:32:18 +0300

    Add AuthController

---
+++ EmployeePostTrace.Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeePostTrace.Api.Models.Requests;
+using EmployeePostTrace.BusinessLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePostTrace.Api.Controllers;
+
+[AllowAnonymous]
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController (IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public async Task<ActionResult<string>> Login([FromBody] LoginRequest loginRequest)
+    {
+        var user = await _authService.Login(loginRequest.Email, loginRequest.Password);
+
+        return Ok(_authService.GetToken(user));
+    }
+}
