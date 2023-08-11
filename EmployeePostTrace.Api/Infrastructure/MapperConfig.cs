4d7311d835d25718d7d21e9470b38b597b8b5fa0
Ilya <ilia.il.27@mail.ru>
2023-08-12 01:27:30 +0300

    Add automapper, DbConfig & UpdateLetterRequest

---
+++ EmployeePostTrace.Api/Infrastructure/MapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using EmployeePostTrace.Api.Models.Requests;
+using EmployeePostTrace.Api.Models.Responses;
+using EmployeePostTrace.DataLayer.Models;
+
+namespace EmployeePostTrace.Api.Infrastructure;
+
+public class MapperConfig : Profile
+{
+    public MapperConfig()
+    {
+        CreateMap<RegistrationEmployeeRequest, EmployeeDto>();
+        CreateMap<UpdateEmployeeRequest, EmployeeDto>();
+        CreateMap<EmployeeDto, EmployeeMainInfoResponse>();
+        CreateMap<EmployeeDto, EmployeeAllInfoResponse>();
+
+        CreateMap<AddLetterRequest, LetterDto>();
+        CreateMap<UpdateLetterRequest, LetterDto>();
+        CreateMap<LetterDto, LetterAllInfoResponse>();
+        CreateMap<LetterDto, LetterMainInfoResponse>();
+
+        CreateMap<LoginRequest, EmployeeDto>();
+    }
+}
---
+++ EmployeePostTrace.Api/Models/Requests/UpdateLetterRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace EmployeePostTrace.Api.Models.Requests;
+
+public class UpdateLetterRequest
+{
+    public string Header { get; set; }
+    public string Content { get; set; }
+}
